bdab312033ada1cbc62f6aea21d4d625667bfa1a
nick123 <147958511+nayan-suresh@users.noreply.github.com>
2025-10-28 16:01:44 +0530

    Refactor shift reducer parser implementation

--- shiftreducer.c
+++ shiftreducer.c
@@ -8,65 +8,61 @@ char ip_sym[15];
 char act[15];
 char temp[2],temp2[2];
 void check();
-void main(){
-printf("\n\tSHIFT REDUCE PARSER\n");
-printf("\nGRAMMAR:\n");
-printf("\nE->E+E\nE->E*E\nE->(E)\nE->a\nE->b\n");
-printf("\nEnter the input Symbol: ");
-scanf("%s",ip_sym);
-printf("\nStack\t\tInput\t\tAction");
-printf("\n------------------------------------");
-printf("\n$\t\t%s$\t\t--",ip_sym);
-while(1){
-temp[0]=ip_sym[ip_ptr];
-temp[1]='\0';
-strcpy(act,"SHIFT ");
-strcat(act,temp);
-len=strlen(ip_sym);
-stack[st_ptr]=ip_sym[ip_ptr];
-stack[st_ptr+1]='\0';
-ip_sym[ip_ptr]=' ';
-printf("\n$%s\t\t%s$\t\t%s",stack,ip_sym,act);
-ip_ptr++;
-st_ptr++;
-check();
-}}
-void check(){
-int flag=0;
-char temp3[15];
-temp2[0]=stack[st_ptr-1];
-temp2[1]='\0';
-if((strcmp(temp2,"a")==0)||(strcmp(temp2,"b")==0)){
-stack[st_ptr-1]='E';
-stack[st_ptr]='\0';
-printf("\n$%s\t\t%s$\t\tREDUCE E->a|b",stack,ip_sym);
-flag=1;
-}else{
-printf("\n$%s\t\t%s$\t\tREDUCE E->b",stack,ip_sym);
-flag=1;
+void main()
+{
+	printf("\n\tSHIFT REDUCE PARSER\n");
+	printf("\nGRAMMAR:\n");
+	printf("E->E+E\nE->E*E\nE->(E)\nE->a|b\n");
+	printf("\n Enter the input Symbol: a+abb\n");
+	scanf("%s",ip_sym);
+	printf("\n Stack\t\tInput\t\tAction\n");
+	printf("\n$ \t\t%s$\t\t--",ip_sym);
+	while(ip_sym[ip_ptr]!='\0')
+	{
+		temp[0]=ip_sym[ip_ptr];
+		temp[1]='\0';
+		strcpy(act,"SHIFT");
+		len=strlen(ip_sym);
+		stack[st_ptr]=ip_sym[ip_ptr];
+		st_ptr++;
+		ip_sym[ip_ptr]=' ';
+		ip_ptr++;
+		printf("\n$%s\t\t%s$\t\t%s",stack,ip_sym,act);
+		check();
+	}
+	check();
 }
-if((strcmp(temp2,"+")==0)||(strcmp(temp2,"*")==0)||(strcmp(temp2,"/")==0))
-flag=1;
-strcpy(temp3,stack);
-strcpy(stack,"E");
-st_ptr=0;
-if(strcmp(temp3,"E+E")==0){
-printf("\n$%s\t\t%s$\t\tREDUCE E->E+E",stack,ip_sym);
-}else if(strcmp(temp3,"E*E")==0){
-printf("\n$%s\t\t%s$\t\tREDUCE E->E*E",stack,ip_sym);
-}else if(strcmp(temp3,"E/E")==0){
-printf("\n$%s\t\t%s$\t\tREDUCE E->E/E",stack,ip_sym);
-}else{
-printf("\n$%s\t\t%s$\t\tREDUCE E->E",stack,ip_sym);
-flag=1;
+
+void check()
+{
+	char temp3[15];
+	if(strcmp(&stack[st_ptr-1],"a")==0 || strcmp(&stack[st_ptr-1],"b")==0)
+	{
+		stack[st_ptr-1]='E';
+		stack[st_ptr]='\0';
+		printf("\n$%s\t\t%s$\tREDUCE E->a|b",stack,ip_sym);
+	}
+	if(st_ptr>=3)
+	{
+		if(stack[st_ptr-3]=='E' && stack[st_ptr-2]=='+' && stack[st_ptr-1]=='E')
+		{
+			st_ptr-=2;
+			stack[st_ptr-1]='E';
+			stack[st_ptr]='\0';
+			printf("\n$%s\t\t%s$\tREDUCE E->E+E",stack,ip_sym);
+		}
+		else if(stack[st_ptr-3]=='E' && stack[st_ptr-2]=='*' && stack[st_ptr-1]=='E')
+		{
+			st_ptr-=2;
+			stack[st_ptr-1]='E';
+			stack[st_ptr]='\0';
+			printf("\n$%s\t\t%s$\tREDUCE E->E*E",stack,ip_sym);
+		}
+	}
+	if(strcmp(stack,"E")==0 && ip_sym[ip_ptr]=='\0')
+	{
+		printf("\n$%s\t\t%s$\tACCEPT",stack,ip_sym);
+		exit(0);
+	}
 }
-if((strcmp(stack,"E")==0)&&ip_ptr==len){
-printf("\n$%s\t\t%s$\t\tACCEPT",stack,ip_sym);
-exit(0);
-}
-if(flag==0){
-printf("\n$%s\t\t%s$\t\tREJECT",stack,ip_sym);
-exit(0);
-}
-return;
-}
+
